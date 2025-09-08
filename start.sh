be9bbcdfef0b1281ac21dc00c9b7814a5a5350cb
YY1104-art <2957266770@qq.com>
2025-09-08 21:43:44 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-
-#!/usr/bin/env bash
-# start both backend and frontend for demo
-uvicorn backend.main:app --host 0.0.0.0 --port 8000 --workers 1 &
-streamlit run frontend/streamlit_app.py --server.port 8501 --server.headless true
